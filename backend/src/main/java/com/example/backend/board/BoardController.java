1f6e98d410f2b0b8370898a960c05b83a3bd1e0e
museongkim <mstar0228@gmail.com>
2025-02-26 09:45:28 +0900

    [FEAT] Board 기능 개발 - BoardController 작성 완료 #1

---
+++ backend/src/main/java/com/example/backend/board/BoardController.java
@@ -0,0 +1,34 @@
+package com.example.backend.board;
+
+import com.example.backend.board.model.BoardDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/product")
+public class BoardController {
+    private final BoardService boardService;
+
+    @PostMapping("/create")
+    public void create(@RequestBody BoardDto.BoardRegister dto) {
+        boardService.create(dto);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<BoardDto.BoardResponse>> list() {
+        List<BoardDto.BoardResponse> response = boardService.list();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{boardIdx}")
+    public ResponseEntity<BoardDto.BoardResponse> read(@PathVariable Long boardIdx) {
+        BoardDto.BoardResponse response = boardService.read(boardIdx);
+
+        return ResponseEntity.ok(response);
+    }
+}
