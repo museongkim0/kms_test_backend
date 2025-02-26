10a35617f569f723bec6e0b0fe3521c27a8e21f8
museongkim <mstar0228@gmail.com>
2025-02-26 09:41:24 +0900

    [FEAT] Board 기능 개발 - BoardDto 작성 완료 #1

---
+++ backend/src/main/java/com/example/backend/board/model/BoardDto.java
@@ -0,0 +1,40 @@
+package com.example.backend.board.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class BoardDto {
+    @Getter
+    public static class BoardRegister {
+        private String title;
+        private String content;
+        private String writer;
+
+        public Board toEntity() {
+            return Board.builder()
+                    .title(title)
+                    .content(content)
+                    .build();
+        }
+    }
+
+    @Getter @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class BoardResponse {
+        private Long idx;
+        private String title;
+        private String content;
+        private String writer;
+
+        public static BoardResponse from(Board board) {
+            return BoardResponse.builder()
+                    .idx(board.getIdx())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .build();
+        }
+    }
+}
