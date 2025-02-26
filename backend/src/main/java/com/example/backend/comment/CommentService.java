005a7da10b8e600eac0f810f7ecb19ef33c89187
museongkim <mstar0228@gmail.com>
2025-02-26 10:33:52 +0900

    [FEAT] Comment 기능 개발 - API 테스트 완료 #2

--- backend/src/main/java/com/example/backend/comment/CommentService.java
+++ backend/src/main/java/com/example/backend/comment/CommentService.java
@@ -1,16 +1,25 @@
 package com.example.backend.comment;
 
+import com.example.backend.board.BoardRepository;
+import com.example.backend.board.BoardService;
 import com.example.backend.board.model.Board;
 import com.example.backend.comment.model.CommentDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final BoardRepository boardRepository;
 
-    public void create(CommentDto.CommentRegister dto, Board board) {
-        commentRepository.save(dto.toEntity(board));
+    public void create(CommentDto.CommentRegister dto, Long boardIdx) {
+        Optional<Board> result = boardRepository.findById(boardIdx);
+        if (result.isPresent()) {
+           Board board = result.get();
+           commentRepository.save(dto.toEntity(board));
+        }
     }
 }
